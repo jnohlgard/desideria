6af3b8cb6ed188aaf3f6b42e975a2f1c1c66519e
Joakim Nohlgård <joakim@nohlgard.se>
2020-05-27 08:33:43 +0200

    riscv: Add define for asm shorthand
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

---
+++ arch/riscv/include/riscv/asm.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
+ */
+#ifndef DESIDERIA_ARCH_RISCV_ASM_H_
+#define DESIDERIA_ARCH_RISCV_ASM_H_
+
+/**
+ * Shorthand notation for better readability
+ *
+ * This is normally available when compiling with GNU extensions but we want to
+ * avoid requiring any extensions if possible
+ */
+#define asm __asm__
+
+#endif // DESIDERIA_ARCH_RISCV_ASM_H_
--- arch/riscv/startup.c
+++ arch/riscv/startup.c
@@ -1,25 +1,27 @@
 /*
  * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
  */
-__attribute__((__naked__, __noreturn__)) void _start() {
+#include "riscv/asm.h"
+
+__attribute__((naked, noreturn )) void _start() {
   /* Clear the return address register to be able to detect the end of 
    * a backtrace. */
-  __asm__ volatile(".cfi_undefined ra");
+  asm volatile(".cfi_undefined ra");
 
   /* Set up the global pointer register, the linker assumes that the gp
    * register always contains the address of the __global_pointer$
    * symbol, so be careful if refactoring this. */
-  __asm__ volatile(".option push;"
+  asm volatile(".option push;"
                    ".option norelax;"
                    "la gp, __global_pointer$;"
                    ".option pop;");
 
   /* Set the default stack pointer */
-  __asm__ volatile("la sp, _stack_end");
+  asm volatile("la sp, _stack_end");
 
   /* Jump to kernel start */
-  __asm__ volatile("tail hang");
+  asm volatile("tail hang");
 
   /* we should never end up here, but spin if we do */
-  __asm__ volatile("1:\nj 1b\n");
+  asm volatile("1:\nj 1b\n");
 }
