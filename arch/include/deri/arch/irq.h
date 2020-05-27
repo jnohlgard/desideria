70cad5c52cd5b1978cdcb337d5132f528d32a4ac
Joakim Nohlgård <joakim@nohlgard.se>
2020-05-27 15:16:48 +0200

    arch/irq: Add ScopedCriticalSection for C++ RAII IRQ management
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- arch/include/deri/arch/irq.h
+++ arch/include/deri/arch/irq.h
@@ -7,4 +7,20 @@
 
 #include "deri/arch/impl/irq_impl.h"
 
+#ifdef __cplusplus
+
+/**
+ * Scoped IRQ disabling lock
+ */
+class ScopedCriticalSection {
+public:
+  inline ScopedCriticalSection() : mask(irq_disable()) {}
+  inline ~ScopedCriticalSection() { irq_restore(mask); }
+
+private:
+  unsigned long mask;
+};
+
+#endif // __cplusplus
+
 #endif // DERI_ARCH_IRQ_H_
