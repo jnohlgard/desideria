bb307b4bf6a5a7b970adb06e5e268d7d34500cc9
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-01 18:03:42 +0200

    riscv: Clean up startup.c
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- arch/riscv/startup.c
+++ arch/riscv/startup.c
@@ -7,15 +7,16 @@
 
 void early_trap();
 
-__attribute__((naked, noreturn)) void _start() {
+__attribute__((naked)) void _start() {
+  /* Clear the return address register to be able to detect the end of
+   * a backtrace. */
+  asm volatile(".cfi_undefined ra");
+
   /* Disable interrupts globally */
   asm volatile("csrc mstatus, %0" ::"I"(MSTATUS_MIE) :);
 
   /* Set up an exception vector for any machine exceptions in early boot */
   asm volatile("csrw mtvec, %0" :: "r"(early_trap));
-  /* Clear the return address register to be able to detect the end of
-   * a backtrace. */
-  asm volatile(".cfi_undefined ra");
 
   /* Set up the global pointer register, the linker assumes that the gp
    * register always contains the address of the __global_pointer$
