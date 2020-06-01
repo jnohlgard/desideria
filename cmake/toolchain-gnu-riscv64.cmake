aba9085fb5cb12a4b226a5b62a097508dbdf1769
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-01 09:31:24 +0200

    cmake: clean up
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- cmake/toolchain-gnu-riscv64.cmake
+++ cmake/toolchain-gnu-riscv64.cmake
@@ -2,10 +2,9 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR riscv)
 
-SET(CMAKE_C_COMPILER "riscv64-unknown-elf-gcc")
-SET(CMAKE_CXX_COMPILER "riscv64-unknown-elf-g++")
+set(CMAKE_C_COMPILER "riscv64-unknown-elf-gcc")
+set(CMAKE_CXX_COMPILER "riscv64-unknown-elf-g++")
 
-#SET(CMAKE_SYSROOT "$ENV{HOME}/Toolchains/sysroots/alpine-armhf")
 # never look in the target sysroot for programs and tools
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # look for libraries and headers on the target sysroot only
