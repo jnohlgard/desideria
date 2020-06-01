1dcc94c9eaef826142842242b4ad1d266126b3cc
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-01 21:19:10 +0200

    core: Call global constructors during init
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- core/init.cpp
+++ core/init.cpp
@@ -16,15 +16,32 @@ extern const uint32_t _data_load[];
 extern uint32_t _data_begin[];
 extern uint32_t _data_end[];
 
+extern "C" typedef void (*init_func_t)();
+extern init_func_t __preinit_array_start[];
+extern init_func_t __preinit_array_end[];
+extern init_func_t __init_array_start[];
+extern init_func_t __init_array_end[];
+
 void deri_init() {
   for (uint32_t *dst = &_bss_begin[0]; dst < &_bss_end[0]; ++dst) {
     *dst = 0UL;
   }
 
   const uint32_t *src = &_data_load[0];
-  for (uint32_t *dst = &_data_begin[0];
-       dst < &_data_end[0]; ++dst, ++src) {
+  for (uint32_t *dst = &_data_begin[0]; dst < &_data_end[0]; ++dst, ++src) {
     *dst = *src;
+  }
+
+  // Call initializer code
+  for (init_func_t *preinit_func = &__preinit_array_start[0];
+       preinit_func < &__preinit_array_end[0]; ++preinit_func) {
+    (*preinit_func)();
+  }
+
+  // Call constructors and other init functions
+  for (init_func_t *init_func = &__init_array_start[0];
+       init_func < &__init_array_end[0]; ++init_func) {
+    (*init_func)();
   }
 
   __extension__ main();
