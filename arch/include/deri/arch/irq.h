c72f16777246c3b496b074fc93015de10cd02e37
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 09:27:24 +0200

    arch: Clean up InterruptGuard declarations
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- arch/include/deri/arch/irq.h
+++ arch/include/deri/arch/irq.h
@@ -16,19 +16,32 @@ namespace deri::arch {
  */
 class InterruptGuard {
 public:
-  __attribute__((always_inline)) inline InterruptGuard()
-      : mask(irq_disable()) {}
-  __attribute__((always_inline)) inline ~InterruptGuard() { irq_restore(mask); }
+  /**
+   * Enter critical section
+   *
+   * This will disable interrupts globally until the object is destructed
+   */
+  InterruptGuard();
+  /**
+   * Leave critical section
+   */
+  ~InterruptGuard();
+
+  // No copying
   InterruptGuard(const InterruptGuard &) = delete;
-  InterruptGuard(InterruptGuard &&) = delete;
   InterruptGuard &operator=(const InterruptGuard &) = delete;
+
+  // Moving would be OK but right now we do not have a use case
+  InterruptGuard(InterruptGuard &&) = delete;
   InterruptGuard &operator=(InterruptGuard &&) = delete;
 
 private:
   const unsigned long mask;
 };
 
-}
+__attribute__((always_inline)) inline InterruptGuard::InterruptGuard() : mask(irq_disable()) {}
+__attribute__((always_inline)) inline InterruptGuard::~InterruptGuard() { irq_restore(mask); }
+} // namespace deri::arch
 
 #endif // __cplusplus
 
