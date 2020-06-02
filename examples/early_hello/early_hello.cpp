ba9cd8453f7cd9d1ce02a3a9a5d92c0be3dc3a5b
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 11:46:57 +0200

    examples/early_hello: Use abstract console for printing
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- examples/early_hello/early_hello.cpp
+++ examples/early_hello/early_hello.cpp
@@ -2,14 +2,13 @@
  * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
  */
 
-#include <deri/dev/sifive_uart.h>
-#include <deri/bsp/memory_map.h>
+#include <deri/console.h>
+
+using deri::console;
 
 int main() {
-  deri::dev::SiFiveUart uart(UART0);
-  uart.init();
   const char msg[] = "Hello world\n";
-  uart.write(msg, sizeof(msg));
+  console->write(msg, sizeof(msg));
   while (1) {
     asm volatile("" ::: "memory");
   }
