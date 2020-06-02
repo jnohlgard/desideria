40cc68bba88adb0b5a356bc770438abebb17deab
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 08:59:13 +0200

    cxxabi: C++ ABI runtime support
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

---
+++ core/cxxabi/cxa_pure_virtual.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
+ */
+
+#include "deri/die.h"
+
+// Pure virtual function calls will end up here.
+// This should only happen if we manage to instantiate an abstract class and
+// call methods on it.
+extern "C" void __cxa_pure_virtual()
+{
+  deri::die(1);
+  while (1) {}
+  __builtin_unreachable();
+}
---
+++ core/cxxabi/operator_delete.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
+ */
+
+#include "deri/die.h"
+#include <cstddef>
+
+// operator delete is required if we use any classes which have virtual
+// destructors, regardless if they are ever heap allocated or not.
+
+// http://en.cppreference.com/w/cpp/memory/new/operator_delete
+void operator delete(void* p)
+{
+  (void)p;
+  deri::die(123);
+}
+
+void operator delete(void* p, size_t t)
+{
+  (void)p;
+  (void)t;
+  deri::die(123);
+}
+
