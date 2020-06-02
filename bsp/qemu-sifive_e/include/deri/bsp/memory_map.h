bf79707aa7851a7da85ff75089d2a3da5df8d3e4
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 11:40:52 +0200

    sifive_e: Add UART1 definition
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- bsp/qemu-sifive_e/include/deri/bsp/memory_map.h
+++ bsp/qemu-sifive_e/include/deri/bsp/memory_map.h
@@ -8,6 +8,9 @@
 #define UART0_BASE 0x10013000
 #define UART0 ((SiFiveUartRegs*)UART0_BASE)
 
+#define UART1_BASE 0x10023000
+#define UART1 ((SiFiveUartRegs*)UART1_BASE)
+
 #define SIFIVE_TEST_REG_BASE 0x00100000
 #define SIFIVE_TEST_REG ((SiFiveTestRegs *)SIFIVE_TEST_REG_BASE)
 
