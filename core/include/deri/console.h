499c9137e97756e243a71c5f225fdd0bd18ce16b
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 11:44:12 +0200

    core: Introduce console handle
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

---
+++ core/include/deri/console.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
+ */
+
+#ifndef DERI_DEV_CONSOLE_H_
+#define DERI_DEV_CONSOLE_H_
+
+#include "deri/dev/char.h"
+
+namespace deri {
+/**
+ * Kernel console device
+ *
+ * Should be set up in early boot by the board initialization code
+ */
+extern dev::CharDev *console;
+}
+#endif // DERI_DEV_CONSOLE_H_
