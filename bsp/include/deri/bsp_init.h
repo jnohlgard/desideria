7198128aba8e592ac0156060793ee3a31d384bcf
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-02 11:41:51 +0200

    bsp: Introduce init()
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

---
+++ bsp/include/deri/bsp_init.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020 Joakim Nohlgård <joakim@nohlgard.se>
+ */
+
+#ifndef DERI_BSP_INIT_H_
+#define DERI_BSP_INIT_H_
+
+#include "deri/dev/char.h"
+
+namespace deri::bsp {
+/**
+ * Board-specific initialization code
+ */
+void init();
+}
+#endif // DERI_BSP_INIT_H_
