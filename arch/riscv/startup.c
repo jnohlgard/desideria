f680b4c7362bac7175a43fbd39b2d0060ead630d
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-03 20:24:54 +0200

    riscv: Clean up _start
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- arch/riscv/startup.c
+++ arch/riscv/startup.c
@@ -9,25 +9,21 @@ void early_trap();
 extern long _stack_end[];
 
 __attribute__((naked)) void _start() {
-  /* Clear the return address register to be able to detect the end of
-   * a backtrace. */
-  asm volatile(".cfi_undefined ra");
-
-  /* Disable interrupts globally */
+  // Disable interrupts globally
   asm volatile("csrc mstatus, %0" ::"rI"(MSTATUS_MIE) :);
 
-  /* Set up an exception vector for any machine exceptions in early boot */
+  // Set up an exception vector for any machine exceptions in early boot
   asm volatile("csrw mtvec, %0" ::"rA"(early_trap));
 
-  /* Set up the global pointer register, the linker assumes that the gp
-   * register always contains the address of the __global_pointer$
-   * symbol, so be careful if refactoring this. */
+  // Set up the global pointer register, the linker assumes that the gp
+  // register always contains the address of the __global_pointer$
+  // symbol, so be careful if refactoring this.
   asm volatile(".option push;"
                ".option norelax;"
                "la gp, __global_pointer$;"
                ".option pop;");
 
-  /* Set the initial stack pointer, using the proper width depending on the ABI */
+  // Set the initial stack pointer, using the proper width depending on the ABI
 #if __LP64__
   asm volatile("ld sp, _initial_sp");
 #else
@@ -36,10 +32,10 @@ __attribute__((naked)) void _start() {
   /* Jump to kernel start */
   asm volatile("tail deri_init");
 
-  /* we should never end up here, but spin if we do */
-  asm volatile("1:\nj 1b\n");
+  // we should never end up here, but spin if we do
+  asm volatile("1: j 1b");
 
-  /* initial stack pointer value */
+  // initial stack pointer value
 #if __LP64__
   asm volatile("_initial_sp:\n.8byte %0" ::"i"(&_stack_end));
 #else
