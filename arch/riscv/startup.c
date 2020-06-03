0ffddc6bb7640e9776ff2f7f2e687eb1cc3aecb4
Joakim Nohlgård <joakim@nohlgard.se>
2020-06-03 12:32:56 +0200

    riscv: Load initial stack pointer from constants instead of immediate
    This allows setting the stack pointer to outside of +/- 2GB from _start
    
    Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

--- arch/riscv/startup.c
+++ arch/riscv/startup.c
@@ -27,12 +27,22 @@ __attribute__((naked)) void _start() {
                "la gp, __global_pointer$;"
                ".option pop;");
 
-  /* Set the default stack pointer */
-  asm volatile("la sp, _stack_end");
-
+  /* Set the initial stack pointer, using the proper width depending on the ABI */
+#if __LP64__
+  asm volatile("ld sp, _initial_sp");
+#else
+  asm volatile("lw sp, _initial_sp");
+#endif
   /* Jump to kernel start */
   asm volatile("tail deri_init");
 
   /* we should never end up here, but spin if we do */
   asm volatile("1:\nj 1b\n");
+
+  /* initial stack pointer value */
+#if __LP64__
+  asm volatile("_initial_sp:\n.8byte %0" ::"i"(&_stack_end));
+#else
+  asm volatile("_initial_sp:\n.4byte %0" ::"i"(&_stack_end));
+#endif
 }
