0ae602a2b6427766909aa67b966f07603a238993
Joakim Nohlgård <joakim@nohlgard.se>
2020-10-14 15:50:41 +0200

    cmake: Refactor back to single tree

--- cmake/deri_common_options.cmake
+++ cmake/deri_common_options.cmake
@@ -1,2 +1,13 @@
-set(CMAKE_EXECUTABLE_SUFFIX .elf)
+if (CMAKE_CROSSCOMPILING)
+  set(CMAKE_EXECUTABLE_SUFFIX .elf)
+endif()
+
 add_link_options(LINKER:--gc-sections -specs=nosys.specs -nostartfiles)
+
+add_compile_options(-fno-common)
+add_compile_options(-fdata-sections -ffunction-sections)
+add_compile_options(-fstack-usage)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit>)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>)
+add_compile_options(-finline-small-functions -findirect-inlining)
