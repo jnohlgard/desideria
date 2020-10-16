2eb861786193925d25267c5af7bd82cb6896c7a8
Joakim Nohlgård <joakim@nohlgard.se>
2020-10-16 21:17:26 +0200

    iob: Use correct types

--- lib/picolibc/iob.cpp
+++ lib/picolibc/iob.cpp
@@ -14,7 +14,7 @@ int iob_console_flush(FILE *);
 
 // Bare minimum stdout
 static FILE __stdio = {
-    .unget = '\0',
+    .unget = decltype(FILE::unget)(0),
     .flags = _FDEV_SETUP_RW,
     .put = iob_console_put,
     .get = iob_console_get,
@@ -30,7 +30,7 @@ FILE *const __iob[3] = {&__stdio, &__stdio, &__stdio};
 namespace {
 
 int iob_console_put(char c, FILE *) {
-  return deri::console->write(&c, sizeof(c));
+  return static_cast<int>(deri::console->write(&c, sizeof(c)));
 }
 int iob_console_get(FILE *) { return 0; }
 
