9e34c5fd3ce151bb6d8cfa82bb72b8325d0034b3
Joakim Nohlgård <joakim@nohlgard.se>
2020-10-16 23:01:02 +0200

    sifive_test: use enum class for finish codes

--- dev/sifive_test/sifive_test.cpp
+++ dev/sifive_test/sifive_test.cpp
@@ -17,10 +17,10 @@ namespace {
  *
  * @see https://github.com/qemu/qemu/blob/master/include/hw/riscv/sifive_test.h
  */
-enum {
-  FINISHER_FAIL = 0x3333,
-  FINISHER_PASS = 0x5555,
-  FINISHER_RESET = 0x7777,
+enum class FinishCodes : uint32_t {
+  FAIL = 0x3333,
+  PASS = 0x5555,
+  RESET = 0x7777,
 };
 
 }
@@ -31,12 +31,12 @@ SiFiveTest::SiFiveTest(SiFiveTestRegs *dev) : dev(dev) {}
 
 void SiFiveTest::pass() {
   // Writing to the test device will power off the virtual machine
-  dev->cmd = FINISHER_PASS;
+  dev->cmd = static_cast<uint32_t>(FinishCodes::PASS);
   __builtin_unreachable();
 }
 
 void SiFiveTest::fail(unsigned int code) {
-  dev->cmd = (code << 16) | FINISHER_FAIL;
+  dev->cmd = (code << 16u) | static_cast<uint32_t>(FinishCodes::FAIL);
   __builtin_unreachable();
 }
 
