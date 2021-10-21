e77d8bde2f71348bb25d4e21de62f32ad311c0cc
Joakim Nohlgård <joakim@nohlgard.se>
2021-10-21 10:24:32 +0200

    gd32: Add RCU driver

---
+++ dev/clock/gd32/rcu_gd32.cpp
@@ -0,0 +1,11 @@
+#include "deri/dev/clock_gd32.h"
+#include "deri/mmio/peripherals.hpp"
+
+namespace deri::dev::clock {
+
+using mmio::RCU;
+void enableModules(AHBEN_bits bits) { RCU.AHBEN |= bits; }
+void enableModules(APB1EN_bits bits) { RCU.APB1EN |= bits; }
+void enableModules(APB2EN_bits bits) { RCU.APB2EN |= bits; }
+
+}  // namespace deri::dev::clock
---
+++ dev/include/deri/dev/clock_gd32.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Joakim Nohlgård
+ */
+
+#pragma once
+
+#include "deri/mmio/bits/RCU_bits.hpp"
+
+namespace deri::dev::clock {
+
+using AHBEN_bits = mmio::RCU_regs::AHBEN_bits;
+using APB1EN_bits = mmio::RCU_regs::APB1EN_bits;
+using APB2EN_bits = mmio::RCU_regs::APB2EN_bits;
+
+  void enableModules(AHBEN_bits bits);
+  void enableModules(APB1EN_bits bits);
+  void enableModules(APB2EN_bits bits);
+
+
+}  // namespace deri::dev::clock
