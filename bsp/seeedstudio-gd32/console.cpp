6ac4f06fd3ff016652340d9bd9eef11ff79dc20a
Joakim Nohlgård <joakim@nohlgard.se>
2021-10-21 10:26:44 +0200

    seeedstudio-gd32: Initialize USART0 pins in console_init

--- bsp/seeedstudio-gd32/console.cpp
+++ bsp/seeedstudio-gd32/console.cpp
@@ -5,11 +5,17 @@
 #include "deri/bsp/console.h"
 
 #include "deri/bsp/devices.h"
-#include "deri/console.h"
+#include "deri/dev/clock_gd32.h"
 
 namespace deri {
+using namespace dev::clock;
+using PinNum = dev::gpio::GpioGd32::PinNum;
 namespace bsp {
-void console_init() { usart0.init(); }
+void console_init() {
+  gpioa.initOutAfio(static_cast<PinNum>(9));
+  enableModules(APB2EN_bits::USART0EN);
+  uart0.init();
+}
 }  // namespace bsp
-bsp::Console console{bsp::usart0};
+bsp::Console console{bsp::uart0};
 }  // namespace deri
