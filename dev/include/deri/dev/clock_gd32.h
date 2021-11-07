d8c860bc19829259bfdd1ec3b22b35b913692ebf
Joakim Nohlgård <joakim@nohlgard.se>
2021-11-07 23:32:41 +0100

    clock_gd32: Apply clang-format

--- dev/include/deri/dev/clock_gd32.h
+++ dev/include/deri/dev/clock_gd32.h
@@ -12,9 +12,8 @@ using AHBEN_bits = mmio::RCU_regs::AHBEN_bits;
 using APB1EN_bits = mmio::RCU_regs::APB1EN_bits;
 using APB2EN_bits = mmio::RCU_regs::APB2EN_bits;
 
-  void enableModules(AHBEN_bits bits);
-  void enableModules(APB1EN_bits bits);
-  void enableModules(APB2EN_bits bits);
-
+void enableModules(AHBEN_bits bits);
+void enableModules(APB1EN_bits bits);
+void enableModules(APB2EN_bits bits);
 
 }  // namespace deri::dev::clock
