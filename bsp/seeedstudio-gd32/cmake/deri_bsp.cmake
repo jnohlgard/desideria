dac03be03e6d2f44b1371cab9cbdc8bd67902cc7
Joakim Nohlgård <joakim@nohlgard.se>
2021-11-14 08:03:33 +0100

    cmake: Clean up ld script arguments

--- bsp/seeedstudio-gd32/cmake/deri_bsp.cmake
+++ bsp/seeedstudio-gd32/cmake/deri_bsp.cmake
@@ -1,3 +1,3 @@
 set(SOC gd32vf1)
 set(SOC_MODEL gd32vf103vbt6)
-target_link_libraries(deri_soc_flags INTERFACE "-T seeedstudio-gd32-periph.ld")
+target_link_libraries(deri_soc_flags INTERFACE "${CMAKE_CURRENT_LIST_DIR}/../ld/seeedstudio-gd32-periph.ld")
