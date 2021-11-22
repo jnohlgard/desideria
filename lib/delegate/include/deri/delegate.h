b77fc094b46ad1a3acfefce82717cac74470a5ad
Joakim Nohlgård <joakim@nohlgard.se>
2021-11-22 22:09:39 +0100

    delegate: Library for dealing with callbacks

---
+++ lib/delegate/include/deri/delegate.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Joakim Nohlgård
+ */
+
+#pragma once
+
+namespace deri {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#include "delegate/delegate.hpp"
+#pragma GCC diagnostic pop
+}
