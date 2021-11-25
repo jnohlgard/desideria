b8e242ec5440a6d1b2f8a3063a9331cd8de704c4
Joakim Nohlgård <joakim@nohlgard.se>
2021-11-25 10:41:28 +0100

    TimerManager: Basic read-only implementation

--- dev/include/deri/dev/timer_manager.hpp
+++ dev/include/deri/dev/timer_manager.hpp
@@ -4,16 +4,55 @@
 
 #pragma once
 
-template <class TimerDriverType>
+#include <atomic>
+#include <chrono>
+#include <cstdint>
+
+namespace deri::dev {
+
+template <class TimerDriverType, typename CountType = std::uint64_t>
 class TimerManager {
  public:
   using TimerDriver = TimerDriverType;
+  using Count = CountType;
   enum class Frequency : unsigned long;
   static constexpr auto num_channels = TimerDriver::num_channels;
 
-  explicit TimerManager(TimerDriver &timer) : timer(timer) {}
+  explicit TimerManager(TimerDriver &timer, Frequency tick_rate_hz)
+      : timer(&timer), tick_rate_hz{tick_rate_hz}, count(0) {}
+  TimerManager() = default;
+  TimerManager(const TimerManager &) = default;
+  TimerManager &operator=(const TimerManager &rhs) noexcept {
+    if (this == &rhs) {
+      return *this;
+    }
+    timer = rhs.timer;
+    tick_rate_hz = rhs.tick_rate_hz;
+    count.store(rhs.count.load());
+    return *this;
+  }
+
+  void init() {
+    timer->setPeriod(
+        static_cast<typename TimerDriver::Count>(TimerDriver::max_value));
+    timer->setPeriodHandler({[](uintptr_t ctx) {
+                               auto *self =
+                                   reinterpret_cast<decltype(this)>(ctx);
+                               self->onRollover();
+                             },
+                             reinterpret_cast<uintptr_t>(this)});
+    timer->start();
+  }
+
+  Count read() const {
+    return count + static_cast<Count>(timer->read());
+  }
 
  private:
-  TimerDriver timer;
-  Frequency tick_rate_hz;
+  void onRollover() { count += static_cast<Count>(TimerDriver::max_value) + 1; }
+
+  TimerDriver *timer{nullptr};
+  Frequency tick_rate_hz{};
+  std::atomic_uint64_t count{};
 };
+}  // namespace deri::dev
