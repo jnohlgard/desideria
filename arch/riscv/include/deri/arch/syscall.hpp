d3623ddc821fbe755545ad4d30a116d3699d39ec
Joakim Nohlgård <joakim@nohlgard.se>
2021-12-11 03:21:01 +0100

    riscv: Fix syscall being mistaken as noreturn by GCC

--- arch/riscv/include/deri/arch/syscall.hpp
+++ arch/riscv/include/deri/arch/syscall.hpp
@@ -10,15 +10,17 @@
 #include <type_traits>
 
 namespace deri::arch {
-#pragma GCC diagnostic push
 // Return value will be set by the ecall handler
-#pragma GCC diagnostic ignored "-Wreturn-type"
 // I have not been able to figure out how to turn this into a variadic template
 // while reliably matching ABI with trap.S
 inline uintptr_t syscall(Syscall syscall_number) {
   auto register syscall_number_reg asm("a7") =
       static_cast<std::underlying_type_t<Syscall>>(syscall_number);
-  asm volatile("ecall" ::"r"(syscall_number_reg) : "memory");
+  uintptr_t register return_value asm("a0");
+  asm volatile("ecall"
+               : "=r"(return_value)
+               : "r"(syscall_number_reg)
+               : "memory");
+  return return_value;
 }
-#pragma GCC diagnostic pop
 }  // namespace deri::arch
