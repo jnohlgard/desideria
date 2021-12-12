c172596304ee1b18d3ae023451156345e0b38d1f
Joakim Nohlgård <joakim@nohlgard.se>
2021-12-12 02:45:12 +0100

    riscv: Use pseudo-instructions for perf reading

--- arch/riscv/include/deri/arch/perf.hpp
+++ arch/riscv/include/deri/arch/perf.hpp
@@ -12,15 +12,15 @@ class Perf {
   [[nodiscard, gnu::always_inline]] static uint64_t cycles() {
     uint32_t lo;
     uint32_t hi;
-    asm volatile("csrr %0, cycle" : "=r"(lo)::);
-    asm volatile("csrr %0, cycleh" : "=r"(hi)::);
+    asm volatile("rdcycle %0" : "=r"(lo)::);
+    asm volatile("rdcycleh %0" : "=r"(hi)::);
     return (static_cast<uint64_t>(hi) << 32 | lo);
   }
   [[nodiscard, gnu::always_inline]] static uint64_t instructionsRetired() {
     uint32_t lo;
     uint32_t hi;
-    asm volatile("csrr %0, instret" : "=r"(lo)::);
-    asm volatile("csrr %0, instreth" : "=r"(hi)::);
+    asm volatile("rdinstret %0" : "=r"(lo)::);
+    asm volatile("rdinstreth %0" : "=r"(hi)::);
     return (static_cast<uint64_t>(hi) << 32 | lo);
   }
 };
