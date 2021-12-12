8c3e9a7531a8bead0167a8b930e77ca31c60e62e
Joakim Nohlgård <joakim@nohlgard.se>
2021-12-12 04:13:40 +0100

    cmake: Define SOC, SOC_MODEL preprocessor macros

--- cmake/deri_flags.cmake
+++ cmake/deri_flags.cmake
@@ -5,10 +5,11 @@ target_compile_definitions(deri_base_flags INTERFACE "BOARD_$<UPPER_CASE:$<MAKE_
 target_compile_definitions(deri_base_flags INTERFACE "CPU_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU}>>=\"${CPU}\"" "DERI_CPU=CPU_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU}>>")
 target_compile_definitions(deri_base_flags INTERFACE "ARCH_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${ARCH}>>=\"${ARCH}\"" "DERI_ARCH=ARCH_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${ARCH}>>")
 target_compile_definitions(deri_base_flags INTERFACE "CPU_CORE_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU_CORE}>>=\"${CPU_CORE}\"" "DERI_CPU_CORE=CPU_CORE_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU_CORE}>>")
-target_compile_definitions(deri_base_flags INTERFACE "CPU_MODEL_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU_MODEL}>>=\"${CPU_MODEL}\"" "DERI_CPU_MODEL=CPU_MODEL_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${CPU_MODEL}>>")
+target_compile_definitions(deri_base_flags INTERFACE "SOC_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${SOC}>>=\"${SOC}\"" "DERI_SOC=SOC_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${SOC}>>")
+target_compile_definitions(deri_base_flags INTERFACE "SOC_MODEL_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${SOC_MODEL}>>=\"${SOC_MODEL}\"" "DERI_SOC_MODEL=SOC_MODEL_$<UPPER_CASE:$<MAKE_C_IDENTIFIER:${SOC_MODEL}>>")
 
 target_link_options(deri_base_flags INTERFACE -nostartfiles)
 
 if (DERI_DEVELHELP)
   target_compile_definitions(deri_base_flags INTERFACE DEVELHELP)
-endif()
+endif ()
