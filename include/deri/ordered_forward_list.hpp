d8ca1992d94f8cd5e35aeda9fc3ab272553ce467
Joakim Nohlgård <joakim@nohlgard.se>
2021-12-23 08:27:24 +0100

    OrderedForwardList: Use composition for cleaner encapsulation

--- include/deri/ordered_forward_list.hpp
+++ include/deri/ordered_forward_list.hpp
@@ -11,11 +11,11 @@
 namespace deri {
 
 template <typename ValueType, class Compare = std::less<ValueType>>
-class OrderedForwardList : public ForwardList<ValueType> {
+class OrderedForwardList {
  public:
   using Value = ValueType;
-  using Node = ForwardListNode<Value>;
-  using Iterator = typename Node::Iterator;
+  using Iterator = typename ForwardList<ValueType>::Iterator;
+  using ConstIterator = typename ForwardList<ValueType>::ConstIterator;
 
   // We disallow copying because the heads of the lists would get out of sync.
   OrderedForwardList() : OrderedForwardList(Compare{}){};
@@ -23,27 +23,33 @@ class OrderedForwardList : public ForwardList<ValueType> {
   OrderedForwardList &operator=(const OrderedForwardList &) = delete;
   OrderedForwardList(OrderedForwardList &&) noexcept = default;
   OrderedForwardList &operator=(OrderedForwardList &&) noexcept = default;
+
   explicit OrderedForwardList(Compare compare) : compare(compare) {}
+  Iterator begin() const { return list.begin(); }
+  ConstIterator end() const { return ForwardList<ValueType>::end(); }
 
-  void pop() { this->pop_front(); }
+  Value &front() { return list.front(); }
+  const Value &front() const { return list.front(); }
+  [[nodiscard]] bool empty() const { return list.empty(); }
 
-  void push(ValueType &to_insert) { push(to_insert, compare); }
+  void pop() { list.pop_front(); }
+
+  void push(Value &to_insert) { push(to_insert, compare); }
+
+  size_t remove(Value &to_remove) { return list.remove(to_remove); }
 
   template <class Callable>
-  Iterator push(ValueType &to_insert, Callable do_compare) {
-    auto iterator = this->begin();
-    for (; iterator != this->end(); ++iterator) {
-      if (!do_compare(*iterator, to_insert)) {
-        iterator.insertBeforeMe(to_insert);
-        return iterator;
-      }
+  Iterator push(Value &to_insert, Callable do_compare) {
+    auto iterator = list.begin();
+    while (iterator != list.end() && do_compare(*iterator, to_insert)) {
+      ++iterator;
     }
-    // The new element is at the end of the list
     iterator.insertBeforeMe(to_insert);
     return iterator;
   }
 
  private:
+  ForwardList<Value> list{};
   [[no_unique_address]] Compare compare{};
 };
 
