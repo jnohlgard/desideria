dc3fd29c8189f1d3e5e38e77a4e7a4f87a2bf589
Joakim Nohlgård <joakim@nohlgard.se>
2021-12-30 07:44:25 +0100

    atomic_impl: Mark all functions as used
    LTO complains about missing symbols without it

--- arch/common/atomic_impl.cpp
+++ arch/common/atomic_impl.cpp
@@ -11,18 +11,21 @@
 // Implementations for GCC atomic helpers
 //
 namespace deri {
+
 template <typename Int>
 Int atomic_load(const volatile void *ptr, int /*memorder*/) {
   arch::CriticalSection cs;
   auto &stored = *reinterpret_cast<const volatile Int *>(ptr);
   return stored;
 }
+
 template <typename Int>
 void atomic_store(volatile void *ptr, Int val, int /*memorder*/) {
   arch::CriticalSection cs;
   auto &stored = *reinterpret_cast<volatile Int *>(ptr);
   stored = val;
 }
+
 template <typename Int, class BinaryOp>
 Int atomic_fetch_op(volatile void *ptr,
                     Int val,
@@ -34,6 +37,7 @@ Int atomic_fetch_op(volatile void *ptr,
   stored = op(old, val);
   return old;
 }
+
 template <typename Int>
 bool atomic_compare_exchange(volatile void *ptr,
                              void *expected_ptr,
@@ -54,44 +58,62 @@ bool atomic_compare_exchange(volatile void *ptr,
     return false;
   }
 }
+
 }  // namespace deri
+
 extern "C" {
 
-uint64_t __atomic_load_8(volatile const void *ptr, int memorder) {
+[[gnu::used]] uint64_t __atomic_load_8(volatile const void *ptr, int memorder) {
   return deri::atomic_load<uint64_t>(ptr, memorder);
 }
-void __atomic_store_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] void __atomic_store_8(volatile void *ptr,
+                                    uint64_t val,
+                                    int memorder) {
   deri::atomic_store(ptr, val, memorder);
 }
-uint64_t __atomic_exchange_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_exchange_8(volatile void *ptr,
+                                           uint64_t val,
+                                           int memorder) {
   return deri::atomic_fetch_op(
       ptr, val, memorder, [](auto lhs, auto rhs) { return rhs; });
 }
-bool __atomic_compare_exchange_8(volatile void *ptr,
-                                 void *expected,
-                                 uint64_t desired,
-                                 bool weak,
-                                 int success_memorder,
-                                 int failure_memorder) {
+[[gnu::used]] bool __atomic_compare_exchange_8(volatile void *ptr,
+                                               void *expected,
+                                               uint64_t desired,
+                                               bool weak,
+                                               int success_memorder,
+                                               int failure_memorder) {
   return deri::atomic_compare_exchange(
       ptr, expected, desired, weak, success_memorder, failure_memorder);
 }
-uint64_t __atomic_fetch_add_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_add_8(volatile void *ptr,
+                                            uint64_t val,
+                                            int memorder) {
   return deri::atomic_fetch_op(ptr, val, memorder, std::plus());
 }
-uint64_t __atomic_fetch_sub_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_sub_8(volatile void *ptr,
+                                            uint64_t val,
+                                            int memorder) {
   return deri::atomic_fetch_op(ptr, val, memorder, std::minus());
 }
-uint64_t __atomic_fetch_and_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_and_8(volatile void *ptr,
+                                            uint64_t val,
+                                            int memorder) {
   return deri::atomic_fetch_op(ptr, val, memorder, std::bit_and());
 }
-uint64_t __atomic_fetch_xor_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_xor_8(volatile void *ptr,
+                                            uint64_t val,
+                                            int memorder) {
   return deri::atomic_fetch_op(ptr, val, memorder, std::bit_xor());
 }
-uint64_t __atomic_fetch_or_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_or_8(volatile void *ptr,
+                                           uint64_t val,
+                                           int memorder) {
   return deri::atomic_fetch_op(ptr, val, memorder, std::bit_or());
 }
-uint64_t __atomic_fetch_nand_8(volatile void *ptr, uint64_t val, int memorder) {
+[[gnu::used]] uint64_t __atomic_fetch_nand_8(volatile void *ptr,
+                                             uint64_t val,
+                                             int memorder) {
   return deri::atomic_fetch_op(
       ptr, val, memorder, [](auto lhs, auto rhs) { return ~(lhs & rhs); });
 }
@@ -106,31 +128,35 @@ uint64_t __atomic_fetch_nand_8(volatile void *ptr, uint64_t val, int memorder) {
 #pragma redefine_extname __atomic_exchange_c __atomic_exchange
 #pragma redefine_extname __atomic_compare_exchange_c __atomic_compare_exchange
 
-void __atomic_load_c(size_t size,
-                     const void *src,
-                     void *dest,
-                     int /*memorder*/) {
+[[gnu::used]] void __atomic_load_c(size_t size,
+                                   const void *src,
+                                   void *dest,
+                                   int /*memorder*/) {
   deri::arch::CriticalSection cs;
   memcpy(dest, src, size);
 }
 
-void __atomic_store_c(size_t size,
-                      void *dest,
-                      const void *src,
-                      int /*memorder*/) {
+[[gnu::used]] void __atomic_store_c(size_t size,
+                                    void *dest,
+                                    const void *src,
+                                    int /*memorder*/) {
   deri::arch::CriticalSection cs;
   memcpy(dest, src, size);
 }
-void __atomic_exchange_c(
+[[gnu::used]] void __atomic_exchange_c(
     size_t size, void *ptr, void *val, void *ret, int /*memorder*/) {
   deri::arch::CriticalSection cs;
   memcpy(ret, ptr, size);
   memcpy(ptr, val, size);
 }
 
-bool __atomic_compare_exchange_c(size_t size, void *ptr, void *expected,
-                                 void *desired, bool /*weak*/, int /*success_memorder*/,
-                                 int /*failure_memorder*/) {
+[[gnu::used]] bool __atomic_compare_exchange_c(size_t size,
+                                               void *ptr,
+                                               void *expected,
+                                               void *desired,
+                                               bool /*weak*/,
+                                               int /*success_memorder*/,
+                                               int /*failure_memorder*/) {
   deri::arch::CriticalSection cs;
   if (memcmp(ptr, expected, size) == 0) {
     memcpy(ptr, desired, size);
