c70bb700c81bc4d36da88a050d1026c5b7c85a93
Joakim Nohlgård <joakim@nohlgard.se>
2022-01-01 14:18:53 +0100

    registers: Provide both atomic and non-atomic implementations

--- dev/include/deri/registers.hpp
+++ dev/include/deri/registers.hpp
@@ -2,9 +2,9 @@
 
 #include "deri/bitwise_enums.hpp"
 
+#include <atomic>
 #include <cstddef>
 #include <cstdint>
-#include <atomic>
 #include <type_traits>
 
 namespace deri::mmio {
@@ -42,9 +42,8 @@ class Reserved {
  * an enum
  */
 template <typename BitsType,
-          typename StorageType =
-              volatile std::atomic<std::underlying_type_t<BitsType>>,
-          typename RawStorageType = typename StorageType::value_type>
+          typename StorageType = std::underlying_type_t<BitsType>,
+          typename RawStorageType = StorageType>
 class Register {
  public:
   // These objects are hardware constructs, a constructor in software does not
@@ -55,30 +54,83 @@ class Register {
   Register &operator=(const Register &) = delete;
   Register &operator=(Register &&) noexcept = delete;
 
-  [[nodiscard]] BitsType load() const { return BitsType{bits.load()}; }
-
-  void store(const BitsType op) { bits.store(static_cast<RawStorageType>(op)); }
-
-  void operator|=(const BitsType op) { fetch_or(op); }
-  void operator&=(const BitsType op) { fetch_and(op); }
-  void operator^=(const BitsType op) { fetch_xor(op); }
-
-  BitsType fetch_or(const BitsType op) {
+  // operators when StorageType has atomic operations:
+  [[nodiscard]] BitsType load() const
+      requires(requires(StorageType bits) { bits.load(); }) {
+    return BitsType{bits.load()};
+  }
+  void store(const BitsType op) requires(requires(StorageType bits,
+                                                  RawStorageType value) {
+    bits.store(value);
+  }) {
+    bits.store(static_cast<RawStorageType>(op));
+  }
+  void operator|=(const BitsType op) requires(requires(StorageType bits) {
+    fetch_or(bits);
+  }) {
+    fetch_or(op);
+  }
+  void operator&=(const BitsType op) requires(requires(StorageType bits) {
+    fetch_and(bits);
+  }) {
+    fetch_and(op);
+  }
+  void operator^=(const BitsType op) requires(requires(StorageType bits) {
+    fetch_xor(bits);
+  }) {
+    fetch_xor(op);
+  }
+  BitsType fetch_or(const BitsType op) requires(requires(StorageType bits,
+                                                         RawStorageType value) {
+    bits.fetch_or(value);
+  }) {
     return BitsType{bits.fetch_or(static_cast<RawStorageType>(op))};
   }
-  BitsType fetch_and(const BitsType op) {
+  BitsType fetch_and(const BitsType op) requires(requires(
+      StorageType bits, RawStorageType value) { bits.fetch_and(value); }) {
     return BitsType{bits.fetch_and(static_cast<RawStorageType>(op))};
   }
-  BitsType fetch_xor(const BitsType op) {
+  BitsType fetch_xor(const BitsType op) requires(requires(
+      StorageType bits, RawStorageType value) { bits.fetch_xor(value); }) {
     return BitsType{bits.fetch_xor(static_cast<RawStorageType>(op))};
+  }
+
+  // Default operators when StorageType does not provide atomic access methods:
+  [[nodiscard]] BitsType load() const { return BitsType{bits}; }
+  void store(const BitsType op) { bits = static_cast<StorageType>(op); }
+  void operator|=(const BitsType op) {
+    auto value = bits;
+    value |= static_cast<StorageType>(op);
+    bits = value;
+  }
+  void operator&=(const BitsType op) {
+    auto value = bits;
+    value &= static_cast<StorageType>(op);
+    bits = value;
+  }
+  void operator^=(const BitsType op) {
+    auto value = bits;
+    value ^= static_cast<StorageType>(op);
+    bits = value;
   }
 
   bool any(const BitsType mask) { return !!(load() & mask); }
 
  private:
-  StorageType bits;
+  volatile StorageType bits;
 };
 
 static_assert(sizeof(Register<std::byte>) == sizeof(std::uint8_t));
 static_assert(sizeof(Reserved<std::byte>) == sizeof(std::uint8_t));
+
+/**
+ * Hardware register that can be accessed using atomic operations
+ */
+template <typename BitsType>
+using AtomicRegister = Register<
+    BitsType,
+    volatile std::atomic<std::underlying_type_t<BitsType>>,
+    typename std::atomic<std::underlying_type_t<BitsType>>::value_type>;
+
+static_assert(sizeof(AtomicRegister<std::byte>) == sizeof(std::uint8_t));
 }  // namespace deri::mmio
