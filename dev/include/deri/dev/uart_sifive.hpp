a9f2694145b50d9854b98a33c38b0adcda65a16c
Joakim Nohlgård <joakim@nohlgard.se>
2022-01-01 14:45:52 +0100

    uart_sifive: Use MMIO headers for registers

--- dev/include/deri/dev/uart_sifive.hpp
+++ dev/include/deri/dev/uart_sifive.hpp
@@ -5,39 +5,51 @@
 #pragma once
 
 #include "deri/registers.hpp"
+#include "deri/mmio/UART.hpp"
 
-#include <atomic>
+#include <optional>
 #include <cstddef>
-#include <cstdint>
 #include <span>
 
 namespace deri::dev::uart {
 
-#pragma GCC diagnostic push
-// Need to suppress this and other similar warnings:
-// 'deri::dev::uart::UartSiFive::rxdata' should be initialized in the member
-// initialization list [-Weffc++]
-#pragma GCC diagnostic ignored "-Weffc++"
-
 class UartSiFive {
-  enum class txctrl_mask : std::uint32_t {
-    txen = 0x00000001,
-    nstop = 0x00000002,
-    txcnt = 0x00070000,
-  };
-  volatile std::atomic_uint32_t txdata;
-  volatile std::uint32_t rxdata;
-  volatile std::uint32_t txctrl;
-  volatile std::uint32_t rxctrl;
-  volatile std::uint32_t ie;
-  volatile std::uint32_t ip;
-  volatile std::uint32_t div;
-
  public:
+  /**
+   * Initialize the hardware module
+   */
   void init();
 
-  auto write(std::span<const std::byte> buffer) -> decltype(buffer);
+  /**
+   * Set the baud rate divider register
+   *
+   * No checking is made to ensure that the desired baud can be reached
+   *
+   * This function must be called every time the peripheral clock frequency is
+   * changed.
+   *
+   * @param[in] pclk peripheral clock (Hz)
+   * @param[in] baudrate desired baud rate (baud)
+   */
+  void setBaud(unsigned pclk, unsigned baudrate);
+
+  /**
+   * Write some bytes to the UART.
+   *
+   * This function will never block and will only write data if there is space
+   * available in the hardware transmit register.
+   *
+   * @param buffer Data to write to the UART
+   * @return @p buffer trimmed to contain only what was not yet written to the
+   * transmit register
+   */
+  [[nodiscard]] auto tryWrite(std::span<const std::byte> buffer)
+      -> decltype(buffer);
+
+  std::optional<std::byte> getRxByte();
+
+ private:
+  mmio::UART_regs UART;
 };
-#pragma GCC diagnostic pop
 
 }  // namespace deri::dev::uart
--- dev/uart/sifive/uart_sifive.cpp
+++ dev/uart/sifive/uart_sifive.cpp
@@ -1,23 +1,42 @@
 #include "deri/dev/uart_sifive.hpp"
 
+#include "deri/mmio/UART.hpp"
+#include "deri/mmio/bits/UART_bits.hpp"
+
+#include <cstdint>
+
 namespace deri::dev::uart {
 
 void UartSiFive::init() {
   // disable interrupts
-  ie = 0;
-  // set divisor
-  div = 0;
-  // enable transmitter
-  txctrl = static_cast<std::uint32_t>(txctrl_mask::txen);
+  UART.ie.store({});
+  UART.txctrl.store(mmio::UART_regs::txctrl_bits::enable);
 }
 
-auto UartSiFive::write(std::span<const std::byte> buffer) -> decltype(buffer) {
+auto UartSiFive::tryWrite(std::span<const std::byte> buffer)
+    -> decltype(buffer) {
   while (!buffer.empty() &&
-      txdata.fetch_or(static_cast<std::uint32_t>(buffer.front())) == 0) {
+         !UART.txdata.fetch_or(mmio::UART_regs::txdata_bits{buffer.front()})) {
     buffer = buffer.last(buffer.size() - 1);
   }
 
   return buffer;
+}
+
+void UartSiFive::setBaud(unsigned int pclk, unsigned int baudrate) {
+  if (pclk == 0) {
+    // Avoid division by 0
+    return;
+  }
+  auto div = pclk / baudrate - 1;
+  UART.div.store(mmio::UART_regs::div_bits{div});
+}
+std::optional<std::byte> UartSiFive::getRxByte() {
+  auto rx = UART.rxdata.load();
+  if (static_cast<uint32_t>(rx) > 0xffu) {
+    return std::nullopt;
+  }
+  return static_cast<std::byte>(rx);
 }
 
 }  // namespace deri::dev::uart
