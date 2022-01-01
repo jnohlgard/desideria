0c6511d1f0b4abd9f02f4abe84a785b8a9bd5e2d
Joakim Nohlgård <joakim@nohlgard.se>
2022-01-01 10:22:03 +0100

    riscv: Read cycle and instret high order word twice to ensure atomicity

--- arch/riscv/include/deri/arch/perf.hpp
+++ arch/riscv/include/deri/arch/perf.hpp
@@ -11,17 +11,29 @@ class Perf {
  public:
   [[nodiscard, gnu::always_inline]] static uint64_t cycles() {
     uint32_t lo;
-    uint32_t hi;
-    asm volatile("rdcycle %0" : "=r"(lo)::);
-    asm volatile("rdcycleh %0" : "=r"(hi)::);
-    return (static_cast<uint64_t>(hi) << 32 | lo);
+    uint32_t hi1;
+    uint32_t hi2;
+    asm volatile(
+        "1:\n\t"
+        "rdcycleh %1\n\t"
+        "rdcycle %0\n\t"
+        "rdcycleh %2\n\t"
+        "bne %1, %2, 1b\n\t"
+        : "=r"(lo), "=r"(hi1), "=r"(hi2)::);
+    return (static_cast<uint64_t>(hi1) << 32 | lo);
   }
   [[nodiscard, gnu::always_inline]] static uint64_t instructionsRetired() {
     uint32_t lo;
-    uint32_t hi;
-    asm volatile("rdinstret %0" : "=r"(lo)::);
-    asm volatile("rdinstreth %0" : "=r"(hi)::);
-    return (static_cast<uint64_t>(hi) << 32 | lo);
+    uint32_t hi1;
+    uint32_t hi2;
+    asm volatile(
+        "1:\n\t"
+        "rdinstreth %1\n\t"
+        "rdinstret %0\n\t"
+        "rdinstreth %2\n\t"
+        "bne %1, %2, 1b\n\t"
+        : "=r"(lo), "=r"(hi1), "=r"(hi2)::);
+    return (static_cast<uint64_t>(hi1) << 32 | lo);
   }
 };
 }  // namespace deri::arch
